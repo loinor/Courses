7a72a5282e4a3e63b3f2cba083975e64af869090
Viacheslav Guskov <gertsslavik@yandex.ru>
2020-07-26 15:07:55 +0300

    Backup for test

---
+++ Udemy/React + Redux/react-quiz/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/layout';
+import Quiz from './containers/Quiz/Quiz';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Quiz></Quiz>
+      </Layout>
+    );
+  }
+}
+
+export default App;
